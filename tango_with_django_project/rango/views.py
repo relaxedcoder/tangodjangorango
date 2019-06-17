c1a0b9879748e5b7297d805e4c617ffbcc46f2d6
srivastava9 <krishu4626@gmail.com>
2019-06-17 16:29:02 +0530

    models created

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 def index(request):
-    return HttpResponse("Rango says hi there!")
+    context_dict={'boldmessages':"crunchy,creamy,softy,adi"}
+    return render(request,'rango/student_profile.html',context=context_dict)
+def doubt(request):
+    return render(request,'rango/doubt.html')
+def greet(request):
+    return HttpResponse(" <a href='/rango/'>Hey </a>how do yo do welcome to django tutorial")
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -14,8 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
+STATIC_DIR=os.path.join(BASE_DIR,'static')
+print(STATIC_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -53,10 +54,11 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,5 +120,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATICFILES_DIRS=[STATIC_DIR,]
 
 STATIC_URL = '/static/'
